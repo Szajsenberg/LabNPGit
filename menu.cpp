868535613672e04e2e09a63763903dd4ba849c98
Norbert Muzolf <nmugol000@gmail.com>
2023-01-17 16:54:44 +0100

    budowa ciał funkcji

--- menu.cpp
+++ menu.cpp
@@ -1,6 +1,10 @@
 #include "menu.h"
+#include <iostream>
 
 void menu()
 {
-    
+    std::cout<<"1 - Dodawanie\n";
+    std::cout<<"2 - Odejmowanie\n";
+    std::cout<<"3 - Mnozenie\n";
+    std::cout<<"4 - Dzielenie\n";
 }
